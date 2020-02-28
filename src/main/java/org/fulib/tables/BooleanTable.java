b258367cef2005281d2b327400e69619b49bd511
Adrian Kunz <clashsoft@hotmail.com>
2020-02-28 18:10:22 +0100

    Add JavaDoc to BooleanTable

--- src/main/java/org/fulib/tables/BooleanTable.java
+++ src/main/java/org/fulib/tables/BooleanTable.java
@@ -1,6 +1,8 @@
 package org.fulib.tables;
 
 /**
+ * A specialization of the {@link Table} class for {@link Boolean} cell values.
+ *
  * @since 1.2
  */
 public class BooleanTable extends Table<Boolean>
@@ -19,16 +21,27 @@ public class BooleanTable extends Table<Boolean>
 
    // =============== Methods ===============
 
+   /**
+    * @return {@code true} if all cell values in the column pointed to by this table are {@code true},
+    * or this table has no rows
+    */
    public boolean all()
    {
       return this.stream().allMatch(Boolean::booleanValue);
    }
 
+   /**
+    * @return {@code true} if at least one cell value in the column pointed to by this table is {@code true}
+    */
    public boolean any()
    {
       return this.stream().anyMatch(Boolean::booleanValue);
    }
 
+   /**
+    * @return {@code true} if none of the cell values in the column pointed to by this table are {@code true},
+    * or this table has no rows
+    */
    public boolean none()
    {
       return this.stream().noneMatch(Boolean::booleanValue);
