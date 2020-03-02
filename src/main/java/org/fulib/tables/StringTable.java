f951257ab2fd3f23e0fbf3fe85e16408b46f9927
Adrian Kunz <clashsoft@hotmail.com>
2020-03-02 16:58:51 +0100

    Add JavaDoc in StringTable

--- src/main/java/org/fulib/tables/StringTable.java
+++ src/main/java/org/fulib/tables/StringTable.java
@@ -2,6 +2,9 @@ package org.fulib.tables;
 
 import java.util.stream.Collectors;
 
+/**
+ * A specialization of the {@link Table} class for {@link String} cell values.
+ */
 public class StringTable extends PrimitiveTable<String>
 {
    // =============== Constructors ===============
@@ -18,6 +21,17 @@ public class StringTable extends PrimitiveTable<String>
 
    // =============== Methods ===============
 
+   /**
+    * Joins the cell values of the column this table points to with the given separator between items.
+    *
+    * @param separator
+    *    the separator, can be empty but must not be {@code null}
+    *
+    * @return a string containing the values joined with the separator in between
+    *
+    * @throws NullPointerException
+    *    if the separator is {@code null}
+    */
    public String join(String separator)
    {
       return this.stream().collect(Collectors.joining(separator));
