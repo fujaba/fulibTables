1a620eab2a66c1917809df8a27ea8a58d1c7b81a
Adrian Kunz <clashsoft@hotmail.com>
2020-03-02 17:11:07 +0100

    Add JavaDoc to Renderer

--- src/main/java/org/fulib/tables/Renderer.java
+++ src/main/java/org/fulib/tables/Renderer.java
@@ -2,8 +2,24 @@ package org.fulib.tables;
 
 import java.io.IOException;
 
+/**
+ * Instances of this interface can render {@link Table} to strings or {@link Appendable}s.
+ *
+ * @since 1.2
+ */
 public interface Renderer
 {
+   /**
+    * Renders the given table as a string.
+    * <p>
+    * Defaults to {@link #render(Table, Appendable)}.
+    * This method should not be overriden.
+    *
+    * @param table
+    *    the table
+    *
+    * @return the rendered string
+    */
    default String render(Table<?> table)
    {
       final StringBuilder builder = new StringBuilder();
@@ -17,5 +33,16 @@ public interface Renderer
       return builder.toString();
    }
 
+   /**
+    * Renders the given table into the given appendable.
+    *
+    * @param table
+    *    the table to render
+    * @param builder
+    *    the appendable into which should be rendered
+    *
+    * @throws IOException
+    *    if it occurs when {@linkplain Appendable#append(CharSequence) appending} to the appendable
+    */
    void render(Table<?> table, Appendable builder) throws IOException;
 }
